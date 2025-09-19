edf6d78e1784a1cb596a86f9ea908b1badc08cf4
Cameron Lee <cameronkmlee10@gmail.com>
2025-09-18 22:16:55 -0400

    Add float suffix to prevent potential implicit narrowing

--- lm75bd/lm75bd.c
+++ lm75bd/lm75bd.c
@@ -40,7 +40,7 @@ error_code_t readTempLM75BD(uint8_t devAddr, float *temp) {
 
   int16_t tempData = (int16_t) ((tempBuffer[0] << 8) | tempBuffer[1]) >> 5;
 
-  *temp = tempData * 0.125;
+  *temp = tempData * 0.125f;
 
   return ERR_CODE_SUCCESS;
 }
